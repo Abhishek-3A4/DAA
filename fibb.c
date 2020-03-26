a253fd9690720a6be7448c4bb2d39b0ea88d19a1
abhishekbms <54938226+abhishekbms@users.noreply.github.com>
2020-03-26 17:36:32 +0530

    Add files via upload

--- fibb.c
+++ fibb.c
@@ -2,18 +2,18 @@
 #include<stdlib.h>
 int fib(int n)
 {
-if(n==0)
-return 0;
-else if(n==1)
-return 1;
-else
-return(fib(n-1)+fib(n-2));
+	if(n==1)
+		return 0;
+	if(n==2)
+		return 1;
+	return fib(n-1)+fib(n-2);
 }
-void main()
+int main()
 {
-int n;
-printf("\n Enter value of n:");
-scanf("%d",&n);
-printf("\n%d th element in fibonacci series is %d",n,fib(n));
-return;
+	int n;
+	printf("Enter the value of n:\n");
+	scanf("%d",&n);
+	fib(n);
+	printf("Nth fibonnacci no is %d",fib(n));
+	return 0;
 }
